3d15a7716e6c1908a2fb9e4b79dec3fd539fd84b
Rameez Khan <rameezkhan.sa@gmail.com>
2019-02-27 14:44:57 +0200

    Create an action to save a job offer

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,63 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  console.log("Request headers: " + JSON.stringify(request.headers));
+  console.log("Request body: " + JSON.stringify(request.body));
+
+  // An action is a string used to identify what needs to be done in fulfillment
+  let action = request.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
+  console.log("Actions = " + JSON.stringify(action));
+
+  let query = request.body.result.resolvedQuery;
+
+  // Parameters are any entites that Dialogflow has extracted from the request.
+  const parameters = request.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
+
+  // Contexts are objects used to track and store conversation state
+  const inputContexts = request.body.result.contexts; // https://dialogflow.com/docs/contexts
+
+  if (action === "firebase.saveJobOffer") {
+    admin
+      .firestore()
+      .collection("job-offers")
+      .add({
+        test: "test",
+      })
+      .then(ref => {
+        sendResponse("Saved job offer");
+        return ref;
+      })
+      .catch(err => console.log(err));
+  }
+
+  // Function to send correctly formatted responses to Dialogflow which are then sent to the user
+  function sendResponse(responseToUser) {
+    // if the response is a string send it as a response to the user
+    if (typeof responseToUser === "string") {
+      let responseJson = {};
+      responseJson.speech = responseToUser; // spoken response
+      responseJson.displayText = responseToUser; // displayed response
+      response.json(responseJson); // Send response to Dialogflow
+    } else {
+      // If the response to the user includes rich responses or contexts send them to Dialogflow
+      let responseJson = {};
+
+      // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
+      responseJson.speech = responseToUser.speech || responseToUser.displayText;
+      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+
+      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+      responseJson.data = responseToUser.richResponses;
+
+      // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      responseJson.contextOut = responseToUser.outputContexts;
+
+      response.json(responseJson); // Send response to Dialogflow
+    }
+  }
+});
