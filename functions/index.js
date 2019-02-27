480264655ac41d5b955d7ebbfe4e8cf3b70c1ff3
Rameez Khan <rameezkhan.sa@gmail.com>
2019-02-27 15:07:12 +0200

    Fix incorrected object reference

--- functions/index.js
+++ functions/index.js
@@ -10,16 +10,16 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   console.log("Request body: " + JSON.stringify(request.body));
 
   // An action is a string used to identify what needs to be done in fulfillment
-  let action = request.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
+  let action = request.body.queryResult.action; // https://dialogflow.com/docs/actions-and-parameters
   console.log("Actions = " + JSON.stringify(action));
 
-  let query = request.body.result.resolvedQuery;
+  // let query = request.body.queryResult.resolvedQuery;
 
   // Parameters are any entites that Dialogflow has extracted from the request.
-  const parameters = request.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
+  const parameters = request.body.queryResult.parameters; // https://dialogflow.com/docs/actions-and-parameters
 
   // Contexts are objects used to track and store conversation state
-  const inputContexts = request.body.result.contexts; // https://dialogflow.com/docs/contexts
+  // const inputContexts = request.body.queryResult.contexts; // https://dialogflow.com/docs/contexts
 
   if (action === "firebase.saveJobOffer") {
     admin
